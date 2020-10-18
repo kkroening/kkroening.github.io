546bee58d30e41c1bc691e90fda4b48b937eeb30
Karl Kroening <karlk@kralnet.us>
2020-10-18 17:57:22 -0500

    poibot -> cartpoi

---
+++ cartpoi/precache-manifest.84e13123d1d63427d512e8ec36e72fff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87eb730ab1bf08d317102c32a7de95b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c9a2575f111d954d725b",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b2ba4af4562fc5e4db7d",
+    "url": "./static/css/main.3ec3cee2.chunk.css"
+  },
+  {
+    "revision": "c9a2575f111d954d725b",
+    "url": "./static/js/2.79882bd3.chunk.js"
+  },
+  {
+    "revision": "c65b4d884c46efb2debfbef355cdf13e",
+    "url": "./static/js/2.79882bd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ba4af4562fc5e4db7d",
+    "url": "./static/js/main.b753dda0.chunk.js"
+  },
+  {
+    "revision": "dcf35002136432d21cac",
+    "url": "./static/js/runtime-main.89f967c6.js"
+  }
+]);
