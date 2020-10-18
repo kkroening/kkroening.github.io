d699522c23f3aa0807965a017b57e6257767cc2c
Karl Kroening <karlk@kralnet.us>
2020-10-18 04:29:42 -0500

    Add poibot

---
+++ poibot/precache-manifest.6b6ab69557a106e83c24b9d6757345b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52773582f908cd734ca758858640812c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "75cdac803a9fb94a390d",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "958b4ead1b104769b706",
+    "url": "./static/css/main.3ec3cee2.chunk.css"
+  },
+  {
+    "revision": "75cdac803a9fb94a390d",
+    "url": "./static/js/2.1100e4d6.chunk.js"
+  },
+  {
+    "revision": "c65b4d884c46efb2debfbef355cdf13e",
+    "url": "./static/js/2.1100e4d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "958b4ead1b104769b706",
+    "url": "./static/js/main.b06b0e2c.chunk.js"
+  },
+  {
+    "revision": "dcf35002136432d21cac",
+    "url": "./static/js/runtime-main.89f967c6.js"
+  }
+]);
