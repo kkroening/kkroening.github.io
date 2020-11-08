93f217f0784cb68b7d70796779263f4ee49c035f
Karl Kroening <karlk@kralnet.us>
2020-11-08 00:01:30 -0600

    Add doublecartpoi

---
+++ doublecartpoi/precache-manifest.f008f5fc1d22187b22eecabb0b85e01f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05be53f64a0c78cd4673",
+    "url": "./0b2543d930a6a638844a.module.wasm"
+  },
+  {
+    "revision": "755f78eed6573cd758c0d7ad61682730",
+    "url": "./index.html"
+  },
+  {
+    "revision": "016f0aa202f8fc5c3742",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "36443ddfc5c2c08b1ecd",
+    "url": "./static/css/main.2188b2f0.chunk.css"
+  },
+  {
+    "revision": "016f0aa202f8fc5c3742",
+    "url": "./static/js/2.47a31f7e.chunk.js"
+  },
+  {
+    "revision": "c65b4d884c46efb2debfbef355cdf13e",
+    "url": "./static/js/2.47a31f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05be53f64a0c78cd4673",
+    "url": "./static/js/3.6255b105.chunk.js"
+  },
+  {
+    "revision": "36443ddfc5c2c08b1ecd",
+    "url": "./static/js/main.1be4e2ae.chunk.js"
+  },
+  {
+    "revision": "ccd7b7198900428d3c6a",
+    "url": "./static/js/runtime-main.1b299f11.js"
+  }
+]);
